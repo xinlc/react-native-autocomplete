2bcd7ce3d0a7e3867068e537efb451f483eff4ea
lichao <1160060619@qq.com>
2018-05-24 18:28:11 +0800

    commit autocomplete

--- examples/Basic/App.js
+++ examples/Basic/App.js
@@ -1,25 +1,83 @@
 /**
  * Example
  * Created by Leo on 23/05/2018.
- * @flow
  */
 import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  Dimensions
 } from 'react-native';
 import Autocomplete from 'react-native-autocomplete';
+import PositionData from './PositionData';
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   state = {
+    data: PositionData.list,
+    searchInput: ''
+  };
+  Autocomplete = null;
+
+  _renderItem(data) {
+    console.log(data)
+    const styles = {
+      height: 40,
+      justifyContent: 'center',
+      borderBottomWidth: StyleSheet.hairlineWidth,
+      borderBottomColor: '#F0F1F2',
+    };
+    // const textColor = this.state.tags.some(n => data.name == n.name) ? '#F9CB00' : '#333';
+    const textColor = '#F9CB00';
+    return (
+      <TouchableOpacity
+        style={styles}
+        onPress={() => {
+          this.Autocomplete.cancel();
+        }}
+      >
+        <Text style={{ color: textColor, fontSize: 16, }}>{data.name}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  _renderEmptyResult() {
+    return (
+      <Text>no data</Text>
+    );
   }
 
   render() {
+    const { searchInput } = this.state
+    let data = [];
+    if (searchInput.trim() != '') {
+      const regex = new RegExp(`^${searchInput.trim()}`, 'i');
+      data = this.state.data.filter((n) => {
+        if (n.name.search(regex) >= 0) {
+          return true;
+        } else if (n.PY.findIndex(n => n.search(regex) >= 0) >= 0) {
+          return true;
+        } else if (n.firstPY.findIndex(n => n.search(regex) >= 0) >= 0) {
+          return true;
+        }
+        return false;
+      });
+    }
     return (
       <View style={styles.container}>
-        <Autocomplete />
+        <Autocomplete
+          ref={ref => (this.Autocomplete = ref)}
+          data={data}
+          textInputProps={{
+            maxLength: 100,
+            placeholder: '输入中文/拼音/首字母',
+            onChangeText: text => this.setState({ searchInput: text })
+          }}
+          listStyle={styles.list}
+          renderItem={({ item }) => this._renderItem(item)}
+          renderEmptyResult={() => this._renderEmptyResult()}
+        />
       </View>
     );
   }
@@ -28,8 +86,10 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginTop: 30,
+  },
+  list: {
+    padding: 10,
+    height: Dimensions.get('window').height - 100,
   },
 });
--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -1,46 +1,135 @@
+
 /**
  * Autocomplete
  * Created by Leo on 23/05/2018.
- * @flow
  */
 import React, { PureComponent } from 'react';
 import {
-  StyleSheet,
   View,
   Text,
   TextInput,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
+import PropTypes from 'prop-types';
 
-type Props = {
-  containerStyle?: View.propTypes.style,
-  onChangeText?: (text: string) => void,
-};
+export default class Autocomplete extends PureComponent {
+  static propTypes = {
+    data: PropTypes.array.isRequired,
+    containerStyle: View.propTypes.style,
+    inputContainerStyle: View.propTypes.style,
+    onShowResults: PropTypes.func,
+    renderItem: PropTypes.func,
+    renderSeparator: PropTypes.func,
+    renderTextInput: PropTypes.func,
+    textInputProps: PropTypes.shape({
+      ...TextInput.propTypes,
+    }),
+    listStyle: View.propTypes.style,
+  }; 
 
-type State = {
-};
-
-export default class Autocomplete extends PureComponent<Props, State> {
   static defaultProps = {
-  }
+    data: [],
+    renderItem: item => (<Text>{item}</Text>),
+    renderTextInput: props => (<TextInput {...props} />),
+    renderSeparator: null,
+    textInputProps: {},
+  };
 
   constructor(props) {
     super(props);
+    this.textInput = null;
     this.state = {
+      showResults: false,
     };
   }
 
-  _onChangeText = (text) => {
-    const { onChangeText, } = this.props;
+  blur = () => {
+    const { textInput } = this;
+    textInput && textInput.blur();
+  };
 
-    if (onChangeText) onChangeText(text);
+  focus = () => {
+    const { textInput } = this;
+    textInput && textInput.focus();
+  };
+
+  cancel = () => {
+    this._hideResults();
+  };
+
+  _keyExtractor = (item, index) => index.toString();
+
+  _onChangeText = (e, onChangeText) => {
+    if (!this.state.showResults) {
+      this.setState({
+        showResults: true
+      });
+    }
+    onChangeText && onChangeText(e);
+  };
+
+  _hideResults = () => {
+    this.setState({
+      showResults: false
+    });
+    this.textInput && this.textInput.clear();
+    this.blur();
+  };
+
+  renderTextInput() {
+    const { textInputProps, renderTextInput, } = this.props;
+    const { onChangeText, style, ...rest } = textInputProps;
+    const props = {
+      ref: ref => (this.textInput = ref),
+      style: [styles.inputStyle, style],
+      onChangeText: e => this._onChangeText(e, onChangeText),
+      placeholderTextColor: '#B7B8C0',
+      clearButtonMode: 'while-editing',
+      underlineColorAndroid: 'transparent',
+      ...rest
+    };
+
+    return renderTextInput(props);
+  }
+
+  renderResultList() {
+    const { data, listStyle, renderItem, renderSeparator, renderEmptyResult } = this.props;
+    return (
+      <FlatList
+        style={[styles.list, listStyle]}
+        data={data}
+        initialNumToRender={10}
+        keyExtractor={this._keyExtractor}
+        renderItem={renderItem}
+        ItemSeparatorComponent={renderSeparator}
+        ListEmptyComponent={renderEmptyResult}
+      />
+    );
   }
 
   render() {
-    const { containerStyle, ...rest } = this.props;
+    const { showResults } = this.state;
+    const { containerStyle, inputContainerStyle, onShowResults } = this.props;
+
+    onShowResults && onShowResults(showResults);
+
     return (
       <View style={[styles.container, containerStyle]}>
-        <TextInput {...rest} onChangeText={this._onChangeText} />
-        <Text>123</Text>
+        <View style={[styles.inputContainer, inputContainerStyle]}>
+          {this.renderTextInput()}
+          {
+            showResults ? (
+              <TouchableOpacity onPress={this._hideResults}>
+                <Text style={styles.cancel}>取消</Text>
+              </TouchableOpacity>
+            ) : null
+          }
+        </View>
+        <View>
+          {showResults && this.renderResultList()}
+        </View>
       </View>
     );
   }
@@ -48,8 +137,48 @@ export default class Autocomplete extends PureComponent<Props, State> {
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    height: 50,
-    backgroundColor: '#ccc'
+    flex: 1,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    zIndex: 1,
+  },
+  inputStyle: {
+    flex: 1,
+    height: 35,
+    padding: 0,
+    paddingLeft: 10,
+    borderWidth: 0,
+    borderRadius: 5,
+    fontSize: 14,
+    color: '#333',
+    backgroundColor: '#EBECEE',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 0,
+    paddingVertical: 8,
+    paddingHorizontal: 14,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#b9b9b9',
+    backgroundColor: '#fff',
+  },
+  list: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    marginTop: 0,
+    borderWidth: 0,
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderTopColor: '#e2e2e2',
+    backgroundColor: '#fff'
+  },
+  cancel: {
+    marginLeft: 10,
+    marginRight: 5,
+    fontSize: 14,
+    color: '#333',
   }
 });
